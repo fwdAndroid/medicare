e255948f8b3f6753c8d96c8260d7ad5f8a838d06
fwdAndroid <fwdkaleem@gmail.com>
2024-08-17 11:47:36 +0500

    Final Version

--- lib/uitls/message_utils.dart
+++ lib/uitls/message_utils.dart
@@ -1,8 +1,1 @@
-import 'package:flutter/material.dart';
 
-showMessageBar(String contexts, BuildContext context) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(contexts),
-    duration: Duration(seconds: 3),
-  ));
-}
