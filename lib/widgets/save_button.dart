ba17d980d96591a593f4a5baae3e89844cf76883
fwdAndroid <fwdkaleem@gmail.com>
2024-09-12 15:38:33 +0500

    Fawad

--- lib/widgets/save_button.dart
+++ lib/widgets/save_button.dart
@@ -28,30 +28,3 @@ class SaveButton extends StatelessWidget {
     );
   }
 }
-
-// ignore: must_be_immutable
-class OutlineButton extends StatelessWidget {
-  String title;
-  final void Function()? onTap;
-
-  OutlineButton({super.key, required this.title, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: onTap,
-      style: OutlinedButton.styleFrom(
-          side: BorderSide(color: Color(0xffE94057), width: 1),
-          fixedSize: Size(46, 20),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
-      child: Text(
-        title,
-        style: TextStyle(
-            fontFamily: "Mulish",
-            fontWeight: FontWeight.w600,
-            color: Color(0xffE94057)),
-      ),
-    );
-  }
-}
